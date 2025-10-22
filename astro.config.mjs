5c92b214560a1a21d95dc38963c064affbb5a5f1
DarkBird54 <160795359+DarkBird54@users.noreply.github.com>
2025-10-22 16:30:53 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind(), react()], // <-- Error corregido: Coma añadida aquí
   output: 'server',
-  adapter: vercel()
+ 
 });
